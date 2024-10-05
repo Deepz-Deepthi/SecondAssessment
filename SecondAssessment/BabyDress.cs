5d9f64a97c5094bcab9b309d09ab87a62e6b09a4
Deepz-Deepthi <deepuramesh2003@outlook.com>
2024-10-05 12:38:03 +0530

    Add project files.

---
+++ SecondAssessment/BabyDress.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondAssessment
+{
+    public class BabyDress
+    {
+        public int Size { get; set; }
+        public string Color { get; set; }
+        public string Brand { get; set; }
+        public double Price { get; set; }
+
+
+
+    }
+}
---
+++ SecondAssessment/BabyDressUtility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondAssessment
+{
+    public class BabyDressUtility
+    {
+        public void AddDressToCart(BabyDress dress)
+        {
+            Program.DressesCart.Add(dress);
+        }
+        public bool RemoveDressFromCart(string brand)
+        {
+            foreach (var dress in Program.DressesCart)
+            {
+                if (dress.Brand == brand)
+                {
+                    Program.DressesCart.Remove(dress);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+    }
+}
